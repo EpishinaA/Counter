9447d034ae206f13f4df30c7ec6d9cff0449fbca
Alyona Epishina <pach2105@yandex.ru>
2023-03-23 17:40:35 +0300

    добавлен код для смены картинки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var isSun: Bool = false
 
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var skyImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        skyImageView.image = UIImage(systemName: "moon")
     }
 
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if isSun {
+            skyImageView.image = UIImage(systemName: "moon")
+        } else {
+            skyImageView.image = UIImage(systemName: "sun.max")
+        }
+        isSun.toggle()
+    }
+    
+    @IBAction func textButton(_ sender: Any) {
+        print("Нажатие")
+    }
 }
 
